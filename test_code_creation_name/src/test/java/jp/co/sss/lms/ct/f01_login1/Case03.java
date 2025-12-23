b5ca260d80280c84df727329a84e7c1aaafe49d0
m-iizuka <m.iizuka@ticnet.co.jp>
2025-12-23 15:42:42 +0900

    fix:Case03 受講生 ログイン 正常系 テストコード編集 ・過去レビューの内容を基にテストコードの内容を編集しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,11 +1,8 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
-import java.time.Duration;
-
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -14,10 +11,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -44,29 +38,39 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() throws Exception {
-		// 指定のURLの画面を開く
-		webDriver.get("http://localhost:8080/lms");
-		// 開いたページのキャプチャを取得する
-		File index_access_success = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(index_access_success, new File("evidence/Case01/index_access_success.png"));
+		// 画面遷移
+		goTo("http://localhost:8080/lms");
+		// アクセスが成功しているか確認
+		String url = webDriver.getCurrentUrl();
+		assertTrue(url.contains("/lms/"));
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() throws Exception {
+
 		// ユーザID・パスワード入力欄にDBに登録されていないユーザーを入力し、ログインを実行
-		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
-		webDriver.findElement(By.id("password")).sendKeys("ItTest2023");
-		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.sendKeys("StudentAA01");
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.sendKeys("ItTest2023");
+		WebElement login = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		login.click();
 
 		// ログイン成功による画面遷移が反映されるまで待機
-		WebDriverWait loginWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-		loginWait.until(ExpectedConditions.urlContains("/detail"));
+		pageLoadTimeout(5);
 
-		// ログイン実行結果のキャプチャを取得する
-		File login_success = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(login_success, new File("evidence/Case03/login_success.png"));
+		// ログインが成功しているか確認
+		String url = webDriver.getCurrentUrl();
+		assertTrue(url.contains("/detail"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
