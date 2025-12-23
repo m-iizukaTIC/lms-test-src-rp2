ec4a5190e7362a6fe5b13a84fcc00dcb8609b106
m-iizuka <m.iizuka@ticnet.co.jp>
2025-12-23 16:09:23 +0900

    fix:Case04 よくある質問画面への遷移 テストコード編集 ・過去レビューの内容を基にテストコードの内容を編集しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -3,10 +3,8 @@ package jp.co.sss.lms.ct.f02_faq;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
-import java.time.Duration;
+import java.util.Set;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -15,10 +13,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -45,71 +40,90 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() throws Exception {
-		// 指定のURLの画面を開く
-		webDriver.get("http://localhost:8080/lms");
-		// 開いたページのキャプチャを取得する
-		File index_access_success = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(index_access_success, new File("evidence/Case01/index_access_success.png"));
+		// 画面遷移
+		goTo("http://localhost:8080/lms");
+		// アクセスが成功しているか確認
+		String url = webDriver.getCurrentUrl();
+		assertTrue(url.contains("/lms/"));
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() throws Exception {
+
 		// ユーザID・パスワード入力欄にDBに登録されていないユーザーを入力し、ログインを実行
-		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
-		webDriver.findElement(By.id("password")).sendKeys("ItTest2023");
-		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.sendKeys("StudentAA01");
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.sendKeys("ItTest2023");
+		WebElement login = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		login.click();
 
 		// ログイン成功による画面遷移が反映されるまで待機
-		WebDriverWait loginWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-		loginWait.until(ExpectedConditions.urlContains("/detail"));
+		pageLoadTimeout(5);
 
-		// ログイン実行結果のキャプチャを取得する
-		File login_success = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(login_success, new File("evidence/Case03/login_success.png"));
+		// ログインが成功しているか確認
+		String url = webDriver.getCurrentUrl();
+		assertTrue(url.contains("/detail"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() throws Exception {
-		webDriver.findElement(By.className("dropdown-toggle")).click();
-		webDriver.findElement(By.linkText("ヘルプ")).click();
+
+		// 「機能」＞「ヘルプ」リンクを押下
+		WebElement function = webDriver.findElement(By.className("dropdown-toggle"));
+		function.click();
+		WebElement help = webDriver.findElement(By.linkText("ヘルプ"));
+		help.click();
 
 		// リンク押下による画面遷移が反映されるまで待機
-		WebDriverWait helpWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-		helpWait.until(ExpectedConditions.urlContains("/help"));
+		pageLoadTimeout(5);
 
-		// リンク押下実行結果のキャプチャを取得する
-		File help_select = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(help_select, new File("evidence/Case04/help_select.png"));
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() throws Exception {
+
 		// 現在のウィンドウを保持
 		String currentWindow = webDriver.getWindowHandle();
 
-		webDriver.findElement(By.linkText("よくある質問")).click();
+		// 「よくある質問」リンクを押下
+		WebElement faq = webDriver.findElement(By.linkText("よくある質問"));
+		faq.click();
 
-		// リンク押下によるタブが開くまで待機 (タブが2以上になる)
-		WebDriverWait faqWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-		faqWait.until(driver -> driver.getWindowHandles().size() > 1);
+		// リンク押下によるタブが開くまで待機
+		pageLoadTimeout(5);
+
 		// 新しく開いたタブに切り替え
-		for (String windowHandle : webDriver.getWindowHandles()) {
+		Set<String> handles = webDriver.getWindowHandles();
+		for (String windowHandle : handles) {
 			if (!windowHandle.equals(currentWindow)) {
 				webDriver.switchTo().window(windowHandle);
 			}
 		}
-		// 新しく開いたタブはURLが/faqになっているか？
-		assertTrue(webDriver.getCurrentUrl().contains("/faq"));
 
-		// リンク押下実行結果のキャプチャを取得する
-		File heq_select = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(heq_select, new File("evidence/Case04/faq_select.png"));
+		// 新しく開いたタブはURLが/faqになっているか？
+		String url = webDriver.getCurrentUrl();
+		assertTrue(url.contains("/faq"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
