6c45d9de789f25defbda41ddc133b6207dd30cbf
m-iizuka <m.iizuka@ticnet.co.jp>
2025-12-23 13:03:06 +0900

    fix:Case02 ログイン画面への遷移 レビュー対応に伴うテストコード編集 ・テストコードの内容をWedDriverUtils準拠の形式に編集しました。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,11 +1,8 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
-import java.time.Duration;
-
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -14,10 +11,6 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト ログイン機能①
@@ -44,11 +37,11 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() throws Exception {
-		// 指定のURLの画面を開く
-		webDriver.get("http://localhost:8080/lms");
-		// 開いたページのキャプチャを取得する
-		File index_access_success = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(index_access_success, new File("evidence/Case01/index_access_success.png"));
+		// 画面遷移
+		goTo("http://localhost:8080/lms");
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -62,12 +55,16 @@ public class Case02 {
 		webDriver.findElement(By.cssSelector(".btn.btn-primary")).click();
 
 		// ログイン失敗によるエラーメッセージが反映されるまで待機
-		WebDriverWait loginWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-		loginWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".help-inline.error")));
+		visibilityTimeout(By.cssSelector(".help-inline.error"), 10);
+
+		// 正しいエラーメッセージが表示されているか？
+		assertEquals("* ログインに失敗しました。", webDriver
+				.findElement(By.cssSelector(".help-inline.error"))
+				.getText());
 
 		// ログイン実行結果のキャプチャを取得する
-		File login_false = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(login_false, new File("evidence/Case02/login_false.png"));
+		getEvidence(new Object() {
+		});
 	}
 
 }
