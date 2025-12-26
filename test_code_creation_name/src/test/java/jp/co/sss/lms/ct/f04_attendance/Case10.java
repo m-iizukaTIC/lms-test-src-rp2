1944d0582cd7036f77aa58f37681a6bfdf4a94e1
m-iizuka <m.iizuka@ticnet.co.jp>
2025-12-26 11:14:32 +0900

    fix:Case10 受講生 勤怠登録 正常系 テストコード編集 ・「勤怠」リンクを押下し、URLをWebElementで取得し勤怠管理画面に遷移できているか確認するテストを作成中しました。(※未完成) ・「出勤」ボタンを押下し、本日の出勤情報が登録されているか確認するテストを作成中です。(※未完成) ・「退勤」ボタンを押下し、本日の退勤情報が登録されているか確認するテストを作成中です。(※未完成)

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -34,36 +37,96 @@ public class Case10 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+
+		// 画面遷移
+		goTo("http://localhost:8080/lms");
+		// アクセスが成功しているか確認
+		String url = webDriver.getCurrentUrl();
+		assertTrue(url.contains("/lms/"));
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+
+		// ユーザID・パスワード入力欄にDBに登録されているユーザーを入力し、ログインを実行
+		WebElement loginId = webDriver.findElement(By.id("loginId"));
+		loginId.sendKeys("StudentAA01");
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.sendKeys("ItTest2023");
+		WebElement login = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		login.click();
+
+		// ログイン成功による画面遷移が反映されるまで待機
+		pageLoadTimeout(5);
+
+		// ログインが成功しているか確認
+		String url = webDriver.getCurrentUrl();
+		assertTrue(url.contains("/detail"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		// 「勤怠」リンクを押下する
+		WebElement attendance = webDriver.findElement(By.linkText("勤怠"));
+		attendance.click();
+
+		// 「勤怠」リンク押下による画面遷移が反映されるまで待機
+		pageLoadTimeout(5);
+
+		// ダイアログ表示があったら解除しておく
+
+		// 勤怠管理画面に遷移しているか確認
+		String url = webDriver.getCurrentUrl();
+		assertTrue(url.contains("/attendance/detail"));
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+
+		// 「出勤」ボタンを押下
+		WebElement punchIn = webDriver.findElement(By.name("punchIn"));
+		punchIn.click();
+
+		// 確認
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+
+		// 「退勤」ボタンを押下
+		WebElement punchOut = webDriver.findElement(By.name("punchOut"));
+		punchOut.click();
+
+		// 確認
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
